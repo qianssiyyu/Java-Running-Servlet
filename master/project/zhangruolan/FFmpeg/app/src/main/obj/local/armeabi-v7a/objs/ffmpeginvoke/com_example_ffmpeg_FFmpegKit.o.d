d809bd714c4baf728a3ed68d68f9153c32b1c9a0
Kannalan <842443872@qq.com>
2020-12-03 11:58:07 +0800

    ffmpeg
    音频合成部分（未完成）

---
+++ master/project/zhangruolan/FFmpeg/app/src/main/obj/local/armeabi-v7a/objs/ffmpeginvoke/com_example_ffmpeg_FFmpegKit.o.d
@@ -0,0 +1,134 @@
+D:/Andriodfile/FFmpeg/app/src/main/obj/local/armeabi-v7a/objs/ffmpeginvoke/com_example_ffmpeg_FFmpegKit.o: \
+  com_example_ffmpeg_FFmpegKit.c com_example_ffmpeg_FFmpegKit.h ffmpeg.h \
+  config.h cmdutils.h \
+  D:\Android\ffmpeg-snapshot\ffmpeg\libavcodec\avcodec.h \
+  D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\samplefmt.h \
+  D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\avutil.h \
+  D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\common.h \
+  D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\attributes.h \
+  D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\macros.h \
+  D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\version.h \
+  D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\avconfig.h \
+  D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\mem.h \
+  D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\error.h \
+  D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\rational.h \
+  D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\mathematics.h \
+  D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\intfloat.h \
+  D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\log.h \
+  D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\pixfmt.h \
+  D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\buffer.h \
+  D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\cpu.h \
+  D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\channel_layout.h \
+  D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\dict.h \
+  D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\frame.h \
+  D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\hwcontext.h \
+  D:\Android\ffmpeg-snapshot\ffmpeg\libavcodec\bsf.h \
+  D:\Android\ffmpeg-snapshot\ffmpeg\libavcodec\codec_id.h \
+  D:\Android\ffmpeg-snapshot\ffmpeg\libavcodec\codec_par.h \
+  D:\Android\ffmpeg-snapshot\ffmpeg\libavcodec\packet.h \
+  D:\Android\ffmpeg-snapshot\ffmpeg\libavcodec\version.h \
+  D:\Android\ffmpeg-snapshot\ffmpeg\libavcodec\codec.h \
+  D:\Android\ffmpeg-snapshot\ffmpeg\libavcodec\codec_desc.h \
+  D:\Android\ffmpeg-snapshot\ffmpeg\libavfilter\avfilter.h \
+  D:\Android\ffmpeg-snapshot\ffmpeg\libavfilter\version.h \
+  D:\Android\ffmpeg-snapshot\ffmpeg\libavformat\avformat.h \
+  D:\Android\ffmpeg-snapshot\ffmpeg\libavformat\avio.h \
+  D:\Android\ffmpeg-snapshot\ffmpeg\libavformat\version.h \
+  D:\Android\ffmpeg-snapshot\ffmpeg\libswscale\swscale.h \
+  D:\Android\ffmpeg-snapshot\ffmpeg\libswscale\version.h \
+  D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\eval.h \
+  D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\fifo.h \
+  D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\thread.h \
+  D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\threadmessage.h \
+  D:\Android\ffmpeg-snapshot\ffmpeg\libswresample\swresample.h \
+  D:\Android\ffmpeg-snapshot\ffmpeg\libswresample\version.h
+
+com_example_ffmpeg_FFmpegKit.h:
+
+ffmpeg.h:
+
+config.h:
+
+cmdutils.h:
+
+D:\Android\ffmpeg-snapshot\ffmpeg\libavcodec\avcodec.h:
+
+D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\samplefmt.h:
+
+D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\avutil.h:
+
+D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\common.h:
+
+D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\attributes.h:
+
+D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\macros.h:
+
+D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\version.h:
+
+D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\avconfig.h:
+
+D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\mem.h:
+
+D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\error.h:
+
+D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\rational.h:
+
+D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\mathematics.h:
+
+D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\intfloat.h:
+
+D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\log.h:
+
+D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\pixfmt.h:
+
+D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\buffer.h:
+
+D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\cpu.h:
+
+D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\channel_layout.h:
+
+D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\dict.h:
+
+D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\frame.h:
+
+D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\hwcontext.h:
+
+D:\Android\ffmpeg-snapshot\ffmpeg\libavcodec\bsf.h:
+
+D:\Android\ffmpeg-snapshot\ffmpeg\libavcodec\codec_id.h:
+
+D:\Android\ffmpeg-snapshot\ffmpeg\libavcodec\codec_par.h:
+
+D:\Android\ffmpeg-snapshot\ffmpeg\libavcodec\packet.h:
+
+D:\Android\ffmpeg-snapshot\ffmpeg\libavcodec\version.h:
+
+D:\Android\ffmpeg-snapshot\ffmpeg\libavcodec\codec.h:
+
+D:\Android\ffmpeg-snapshot\ffmpeg\libavcodec\codec_desc.h:
+
+D:\Android\ffmpeg-snapshot\ffmpeg\libavfilter\avfilter.h:
+
+D:\Android\ffmpeg-snapshot\ffmpeg\libavfilter\version.h:
+
+D:\Android\ffmpeg-snapshot\ffmpeg\libavformat\avformat.h:
+
+D:\Android\ffmpeg-snapshot\ffmpeg\libavformat\avio.h:
+
+D:\Android\ffmpeg-snapshot\ffmpeg\libavformat\version.h:
+
+D:\Android\ffmpeg-snapshot\ffmpeg\libswscale\swscale.h:
+
+D:\Android\ffmpeg-snapshot\ffmpeg\libswscale\version.h:
+
+D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\eval.h:
+
+D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\fifo.h:
+
+D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\thread.h:
+
+D:\Android\ffmpeg-snapshot\ffmpeg\libavutil\threadmessage.h:
+
+D:\Android\ffmpeg-snapshot\ffmpeg\libswresample\swresample.h:
+
+D:\Android\ffmpeg-snapshot\ffmpeg\libswresample\version.h:
